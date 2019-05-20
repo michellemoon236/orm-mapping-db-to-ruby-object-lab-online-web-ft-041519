12c12d1cc9045c7953c5245759edea289b3b9fb6
Michelle Moon <michellemoon236@gmail.com>
2019-05-20 06:35:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,17 +41,28 @@ class Student
   
   def self.all_students_in_grade_9
       #returns an array of all students in grades 9 (FAILED - 1)
+  end
+  
   def self.students_below_12th_grade
       #returns an array of all students in grades 11 or below (FAILED - 2)
+  end
+  
   def self.all
       #returns all student instances from the db
+  end
+  
   def self.first_X_students_in_grade_10
       #returns an array of the first X students in grade 10 (FAILED - 3)
+  end
+  
   def self.first_student_in_grade_10
       #returns the first student in grade 10 (FAILED - 4)
-    .all_students_in_grade_X
+  end
+  
+  def self.all_students_in_grade_X
       #returns an array of all students in a given grade X (FAILED - 5)
-      
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
